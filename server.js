02c64768c9f94ee0361d4bdfe23cdd22f739ac12
Sam Anderson <samel.2710@gmail.com>
2018-03-17 10:16:55 +0000

    Adding simple helpers

--- server.js
+++ server.js
@@ -7,18 +7,20 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Home Page',
-        welcomeMessage: 'Welcome to the website!',
-        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Welcome to the website!'
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'About Page',
-        currentYear: new Date().getFullYear(),
+        pageTitle: 'About Page'
     });
 });
 
