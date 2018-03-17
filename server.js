b158791a98b43fbf084ee0669ee37f6e85a6ea0b
Sam Anderson <samel.2710@gmail.com>
2018-03-17 20:44:40 +0000

    Adding logger middleware and optional maintenance mode

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
 const express = require('express');
+const fs = require('fs');
 const hbs = require('hbs');
+
+const isMaintenance = false;
 
 const app = new express();
 
+// Register partials directory 
 hbs.registerPartials(__dirname + '/views/partials');
+
+// Use hbs/handlebars
 app.set('view engine', 'hbs');
+
+// Logger middleware
+app.use((req, res, next) => {
+    const now = new Date().toString();
+    const log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + "\n", (err) => {
+        if(err) {
+            console.log('Unabled to append server.log');
+        }
+    });
+    next();
+});
+
+// Site maintenance mode middleware
+app.use((req, res, next) => {
+    if(isMaintenance){
+        res.render('maintenance.hbs');
+    } else {
+        next();
+    }
+});
+
+// Static views
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
